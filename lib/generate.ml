5b1f20fdbf293c00c244180cb8fc82a07b96d3e0
Jules Aguillon <juloo.dsi@gmail.com>
2021-01-06 17:28:57 +0100

    Separate the generate Makefile
    The compiling Makefile (currently Makefile.gen) no longer include and
    generate Makefile.generate.
    
    This was like this because the full list of generated files can be
    computed only after the link step is done.

--- lib/generate.ml
+++ lib/generate.ml
@@ -1,41 +1,48 @@
 open Listm
 
-let paths_of_package all_files package =
-  let all_paths =
-    all_files >>= fun file ->
-    match Fpath.(segs (normalize file)) with
-    | "odocls" :: pkg :: _ when pkg = package -> [Fpath.add_ext "odocl" file]
-    | _ -> []
+let output_dir = function `Html -> "html" | `Latex -> "latex" | `Man -> "man"
+
+let generate_command = function
+  | `Html -> "html-generate"
+  | `Latex -> "latex-generate"
+  | `Man -> "man-generate"
+
+let make_target = function `Html -> "html" | `Latex -> "latex" | `Man -> "man"
+
+let mk_pkg target path =
+  let files =
+    Inputs.find_files path >>= fun p ->
+    if Fpath.has_ext ".odocl" p then [ p ] else []
   in
-  setify all_paths
+  List.map
+    (fun f ->
+      let outputs = List.map Fpath.v (Odoc.generate_targets f target) in
+      Makefile.(
+        concat
+          [
+            phony_rule (make_target target) ~fdeps:outputs [];
+            rule outputs ~fdeps:[ f ]
+              [
+                cmd "odoc" $ generate_command target $ "--output-dir"
+                $ output_dir target $ "$<";
+              ];
+          ]))
+    files
+  |> Makefile.concat
 
-let run path package =
-  let package_makefile = Printf.sprintf "Makefile.%s.generate" package in
+let prelude =
+  let open Makefile in
+  let odoc_css = Fpath.v "html/odoc.css" in
+  concat
+    [
+      phony_rule "default" ~deps:[ "html" ] [];
+      phony_rule "html" ~fdeps:[ odoc_css ] [];
+      rule [ odoc_css ] [ cmd "odoc" $ "support-files" $ "--output-dir" $ "html" ];
+    ]
 
-  let all_files = Inputs.find_files path >>= fun p ->
-    let base, ext = Fpath.split_ext p in
-    if ext = ".odocl" then [ base ] else []
+let run paths =
+  let makefile =
+    let mk target = Makefile.concat (List.map (mk_pkg target) paths) in
+    Makefile.concat [ prelude; mk `Html; mk `Latex; mk `Man ]
   in
-
-  let pkg_files = paths_of_package all_files package in
-
-  let oc = open_out package_makefile in
-
-  let mk format =
-    List.iter
-      (fun f ->
-        let str_format = match format with | `Html -> "html" | `Latex -> "latex" | `Man -> "man" in
-        let targets = Odoc.generate_targets f format in
-        let str = Format.asprintf "%s &: %a\n\todoc %s-generate %a --output-dir %s\n" (String.concat " " targets) Fpath.pp f str_format Fpath.pp f str_format in
-        Printf.fprintf oc "%s" str;
-        let str = Format.asprintf "%s : %s\n" str_format (String.concat " " targets) in
-        Printf.fprintf oc "%s" str
-        ) pkg_files;
-  in
-
-  mk `Html;
-  mk `Latex;
-  mk `Man;
-
-  close_out oc
-
+  Format.printf "%a\n" Makefile.pp makefile
