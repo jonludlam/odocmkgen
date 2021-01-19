c24c9237a2baeae706645a3937c6080a9f0f0ef8
Jules Aguillon <juloo.dsi@gmail.com>
2021-01-19 16:14:13 +0100

    Compute compile-deps only once
    In Compile, a quadratic algorithm was used and in Link, dependencies a
    possibly wrong algorithm was used.
    This solves both problems.

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,31 +1,17 @@
-open Listm
 open Util
 
 (* Rules for compiling cm{t,ti,i} files into odoc files *)
-let compile_fragment all_infos info =
+let compile_fragment (info, deps) =
   (* Get the filename of the output odoc file *)
   let odoc_path = Inputs.compile_target info in
-
-  (* Find by digest the [source_info] for each dependency in our source_info record *)
-  let deps =
-    info.deps >>= fun dep ->
-    try [ List.find (fun x -> x.Inputs.digest = dep.Odoc.c_digest) all_infos ]
-    with Not_found ->
-      Format.eprintf "Warning, couldn't find dep %s of file %a\n"
-        dep.Odoc.c_unit_name Fpath.pp info.inppath;
-      []
-  in
-
   (* Get a list of odoc files for the dependencies *)
   let dep_odocs = List.map Inputs.compile_target deps in
-
   (* Odoc requires the directories in which to find the odoc files of the dependencies *)
   let include_args =
     List.map Fpath.parent dep_odocs
     |> List.sort_uniq Fpath.compare
     |> List.concat_map (fun dir -> [ "-I"; Fpath.to_string dir ])
   in
-
   let open Makefile in
   concat
     [
@@ -46,7 +32,6 @@ let gen inputs =
   let open Makefile in
   concat
     [
-      concat (List.map (compile_fragment inputs) inputs);
+      concat (List.map compile_fragment inputs);
       phony_rule "compile" ~deps:package_rules_s [];
     ]
-
