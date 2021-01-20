8a38371a46615cee43f340a203bc8944372d086d
Jules Aguillon <juloo.dsi@gmail.com>
2021-01-20 17:01:04 +0100

    Compute compile-dependencies sooner
    and remove 'digest' and 'deps' fields from Inputs.t.
    These fields doesn't make sense for mld files and are needed only once.

--- lib/inputs.ml
+++ lib/inputs.ml
@@ -36,7 +36,6 @@ let get_cm_files files =
 (** Get mld files out of a list of files. *)
 let get_mld_files = List.filter (Fpath.has_ext ".mld")
 
-(** Represents the necessary information about a particular compilation unit *)
 type t = {
   name : string;  (** 'Astring' *)
   inppath : Fpath.t;  (** Path to the input file, contains [root]. *)
@@ -45,14 +44,13 @@ type t = {
   reloutpath : Fpath.t;
       (** Relative path to use for output, extension is the same as [inppath].
           May not correspond to a input file in [root]. *)
-  digest : Digest.t;  (** Digest of the compilation unit itself *)
   package : string;  (** Package in which this file lives ("astring") *)
-  deps : Odoc.compile_dep list;  (** dependencies of this file *)
 }
+(** Represents the necessary information about a particular compilation unit *)
 
 let pp fmt x =
-  Format.fprintf fmt "@[<v 2>{ name: %s@,root: %a@,path: %a@,digest: %s@,package:%s }@]"
-    x.name Fpath.pp x.root Fpath.pp x.inppath x.digest x.package
+  Format.fprintf fmt "@[<v 2>{ name: %s@,root: %a@,path: %a@,package:%s }@]"
+    x.name Fpath.pp x.root Fpath.pp x.inppath x.package
 
 let input_file t = t.inppath
 
@@ -66,7 +64,6 @@ let link_target t = Fpath.(v "odocls" // set_ext "odocl" t.reloutpath)
 
 (* Get info given a base file (cmt, cmti or cmi) *)
 let get_cm_info root inppath =
-  let deps = Odoc.compile_deps inppath in
   let package = Fpath.(basename (parent inppath)) in
   let reloutpath =
     match Fpath.relativize ~root inppath with
@@ -75,13 +72,7 @@ let get_cm_info root inppath =
   in
   let fname = Fpath.base reloutpath in
   let name = String.capitalize_ascii Fpath.(to_string (rem_ext fname)) in
-  match List.partition (fun d -> d.Odoc.c_unit_name = name) deps with
-  | [ self ], deps ->
-      let digest = self.c_digest in
-      [ { name; inppath; root; reloutpath; digest; package; deps } ]
-  | _ ->
-      Format.eprintf "Failed to find digest for self (%s)\n%!" name;
-      []
+  { name; inppath; root; reloutpath; package }
 
 let get_mld_info root inppath =
   let package = Fpath.(basename (parent inppath)) in
@@ -95,7 +86,7 @@ let get_mld_info root inppath =
   let outfname = Fpath.v ("page-" ^ Fpath.to_string fname) in
   let name = Fpath.to_string (Fpath.rem_ext outfname) in
   let reloutpath = Fpath.append fparent outfname in
-  [ { name; inppath; root; reloutpath; digest = ""; deps = []; package } ]
+  { name; inppath; root; reloutpath; package }
 
 (** Expect paths like [prefix/lib/package] or [prefix/lib/package/sub_dir].
     These paths are used in Opam and in Dune's _build/install.
@@ -110,11 +101,6 @@ let package_of_path path =
     Some (Fpath.basename parent, Fpath.parent grand_parent)
   else None
 
-let find_inputs root =
-  let files = Fs_util.dir_contents_rec root in
-  (get_cm_files files >>= get_cm_info root)
-  @ (get_mld_files files >>= get_mld_info root)
-
 let split_packages inputs =
   let f inp = function Some lst -> Some (inp :: lst) | None -> Some [ inp ] in
   List.fold_left
@@ -127,10 +113,22 @@ module DigestMap = Map.Make (Digest)
 (** Compute direct compile-dependencies for a list of inputs.
     Returns the list of inputs paired with its dependencies. *)
 let compile_deps inputs =
+  let deps_and_digests =
+    List.map
+      (fun inp ->
+        let deps = Odoc.compile_deps inp.inppath in
+        match List.partition (fun d -> d.Odoc.c_unit_name = inp.name) deps with
+        | [ self ], deps -> Some (self.c_digest, deps)
+        | _ ->
+            Format.eprintf "Failed to find digest for self (%s)\n%!" inp.name;
+            None)
+      inputs
+  in
   let inputs_by_digest =
-    List.fold_left
-      (fun acc inp -> DigestMap.add inp.digest inp acc)
-      DigestMap.empty inputs
+    List.fold_left2
+      (fun acc inp -> function
+        | Some (digest, _) -> DigestMap.add digest inp acc | None -> acc)
+      DigestMap.empty inputs deps_and_digests
   in
   let find_dep inp dep =
     match DigestMap.find_opt dep.Odoc.c_digest inputs_by_digest with
@@ -140,5 +138,13 @@ let compile_deps inputs =
           dep.Odoc.c_unit_name Fpath.pp inp.inppath;
         None
   in
-  let find_deps inp = List.filter_map (find_dep inp) inp.deps in
-  List.map (fun inp -> (inp, find_deps inp)) inputs
+  List.map2
+    (fun inp -> function
+      | Some (_, deps) -> (inp, List.filter_map (find_dep inp) deps)
+      | None -> (inp, []))
+    inputs deps_and_digests
+
+let find_inputs root =
+  let files = Fs_util.dir_contents_rec root in
+  compile_deps (get_cm_files files |> List.map (get_cm_info root))
+  @ (get_mld_files files >>= fun mld -> [ get_mld_info root mld, [] ])
